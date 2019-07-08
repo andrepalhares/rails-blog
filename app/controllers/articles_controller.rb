1225bae167efd0e9915b60d1f4f21e0f502ea21c
andrepalhares <andrepalhares@hotmail.com>
2019-07-08 13:43:29 -0300

    crud ate paginacao

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :destroy, :show]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -12,6 +12,7 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
+    @article.user = User.first #teste
     if @article.save #Se não houver erros e o arquivo for adicionado ao banco
       flash[:notice] = "Article added!"
       redirect_to article_path(@article)
